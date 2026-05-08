0f6320e9ea60e5fa928959c3c9d762806f67d921
tmironovich <taniamironovich@vk.com>
2026-05-08 11:27:58 +0500

    feat: add toReadable function for numbers 0-999

--- src/index.js
+++ src/index.js
@@ -1,3 +1,56 @@
-module.exports = function toReadable(/* number */) {
-  throw new Error('Not implemented');
+module.exports = function toReadable(number) {
+  const words = [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+    'ten',
+    'eleven',
+    'twelve',
+    'thirteen',
+    'fourteen',
+    'fifteen',
+    'sixteen',
+    'seventeen',
+    'eighteen',
+    'nineteen',
+  ];
+  const tens = [
+    '',
+    '',
+    'twenty',
+    'thirty',
+    'forty',
+    'fifty',
+    'sixty',
+    'seventy',
+    'eighty',
+    'ninety',
+  ];
+  const tensIndex = Math.floor(number / 10);
+  const units = number % 10;
+  const hundredsIndex = Math.floor(number / 100);
+  const remainder = number % 100;
+  if (number <= 19) {
+    return words[number];
+  }
+  if (number >= 20 && number <= 99) {
+    if (units === 0) {
+      return `${tens[tensIndex]}`;
+    }
+    return `${tens[tensIndex]} ${words[units]}`;
+  }
+  if (number >= 100 && number < 1000) {
+    if (remainder === 0) {
+      return `${words[hundredsIndex]} hundred`;
+    }
+    return `${words[hundredsIndex]} hundred ${toReadable(remainder)}`;
+  }
+  return '';
 };
